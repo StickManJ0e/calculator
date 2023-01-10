3b742c596e2521fe5e20972e2ad613e2f64d0923
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-11 09:51:31 +1100

    Fix previousButton float conversion

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let currentNumArray = [];
-let previousNum = [];
+let previousNum;
+let currentOperator;
+let previousOperator;
+const operators = ["%", "÷", "x", "-", "+"];
 
 //Initalising variables for calculator buttons and screen displays
 const acButton = document.querySelector('#ac-button');
@@ -12,8 +15,9 @@ const additionButton = document.querySelector('#addition-button');
 const decimalButton = document.querySelector('#decimal-button');
 const equalsButton = document.querySelector('#equals-button');
 const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator')
 const currentDisplay = document.querySelector('#current-input');
-const formerDisplay = document.querySelector('former-input');
+const formerDisplay = document.querySelector('#former-input');
 
 //Function that a number or decimal inputed by calc button displays 
 //on the 'screen' and is push into currentNumArray
@@ -31,15 +35,41 @@ numberButtons.forEach(numberButton => {
         pushInputs(numberButton);
     })
 })
-
 decimalButton.addEventListener('click', () => {
     pushInputs(decimalButton);
 });
 
 //Turns the input array into a float
-function toFloat(numArray) {
-    return parseFloat((numArray.join("")));
+let toFloat = (numArray) => parseFloat((numArray.join("")));
+// function toFloat(numArray) {
+//     return parseFloat((numArray.join("")));
+// }
+
+function fixFloat() {
+    previousNum = parseFloat(previousNum.replace(/[%x÷+-]/g, ""));
 }
+
+//Operator buttons functionality
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', () => {
+        if (currentDisplay.textContent === "" ) {
+            previousOperator = currentOperator;
+            currentOperator = operatorButton.textContent;
+            formerDisplay.textContent = formerDisplay.textContent.replace(previousOperator, currentOperator);
+            return;
+        }
+        else if (formerDisplay.textContent === "") {
+            previousNum = toFloat(currentNumArray);
+            currentOperator = operatorButton.textContent;
+            currentNumArray.length = 0;
+            formerDisplay.textContent = previousNum += currentOperator;
+            currentDisplay.textContent = "";
+            fixFloat();
+        }
+    });
+});
+
+
 
 //Other
 function operate(numA, numB, operator) {
