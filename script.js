44dc6e94f73431d4374e2a645e078b27faa5e12b
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-11 08:41:35 +1100

    Add decimal button functionality

--- script.js
+++ script.js
@@ -15,20 +15,33 @@ const numberButtons = document.querySelectorAll('.number');
 const currentDisplay = document.querySelector('#current-input');
 const formerDisplay = document.querySelector('former-input');
 
+//Function that a number or decimal inputed by calc button displays 
+//on the 'screen' and is push into currentNumArray
+function pushInputs(button) {
+    if (currentDisplay.textContent.length >= 12) {
+        return;
+    }
+    currentDisplay.textContent += button.textContent;
+    currentNumArray.push(button.textContent);
+}
+
+//Number and decimal buttons functionality
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', () => {
-        if (currentDisplay.textContent.length >= 12) {
-            return;
-        }
-        currentDisplay.textContent += numberButton.textContent;
-        currentNumArray.push(numberButton.textContent);
+        pushInputs(numberButton);
     })
 })
 
+decimalButton.addEventListener('click', () => {
+    pushInputs(decimalButton);
+});
+
+//Turns the input array into a float
 function toFloat(numArray) {
     return parseFloat((numArray.join("")));
 }
 
+//Other
 function operate(numA, numB, operator) {
     return operator(numA, numB);
 }
